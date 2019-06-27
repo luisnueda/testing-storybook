fbd0bc71bb07462fc89feb088ed410f512502163
luisnueda <nueda.luis@gmail.com>
2019-06-27 21:46:33 +0200

    add knobs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,11 @@
 import { configure, setAddon, addDecorator } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+import { withKnobs } from "@storybook/addon-knobs/react";
 import JSXAddon from "storybook-addon-jsx";
 
 setAddon(JSXAddon);
 addDecorator(withInfo);
+addDecorator(withKnobs);
 
 const req = require.context("../src", true, /.stories.js$/);
 
--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+import { withKnobs } from "@storybook/addon-knobs";
+import { text } from "@storybook/addon-knobs/react";
+import { color } from "@storybook/addon-knobs/react";
 import { Button } from "./Button";
 
 storiesOf("Button", module)
@@ -9,5 +12,8 @@ storiesOf("Button", module)
       text: "Text to describe the component"
     }
   })
-  .addWithJSX("with background", () => <Button bg="palegoldenrod">Hello world</Button>)
-  .addWithJSX("with background 2", () => <Button bg="green">Hello world2</Button>);
+  .addWithJSX("with background", () => <Button bg={text("bg", `green`)}>Hello world</Button>)
+  .addWithJSX("with background 2", () => <Button bg="green">Hello world2</Button>)
+  .addWithJSX("with background 2", () => (
+    <Button bg={color("bg", "green", "group1")}>Hello world2</Button>
+  ));
